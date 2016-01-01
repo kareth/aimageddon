016392c45ff1f6e1a3261ca211e8739019a0341a
Piotr Żurkowski <kareth92@gmail.com>
2016-01-01 22:51:05 +0100

    Update googletest to match googlemock requirements

--- gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kareth/code/codegeddon/snake/server/gtest
-# Build directory: /Users/kareth/code/codegeddon/snake/server/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
